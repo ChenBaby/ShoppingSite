7941f3998af9bab562207efd2c23aa1a3743d638
ChenBaby <1911478180@qq.com>
2018-10-09 22:42:42 +0800

    new router Login

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,12 @@
             <form action="submitSearch">
                 <el-input prefix-icon="el-icon-search" class="search-input" v-bind:class="{showed: searchInputShow}" v-model="searchtext" v-on:click.native="showInput" placeholder="搜一搜..." v-focus="searchInputShow" v-on:blur="searchInputShow=false" type="search"></el-input>
             </form>
-            <a href="/cart"><i class="el-icon-goods"></i></a>
+            <a href="/login" class="header_account">
+                <svg aria-hidden="true" focusable="false" role="presentation" class="icon icon-login" viewBox="0 0 28.33 37.68">
+                    <path d="M14.17 14.9a7.45 7.45 0 1 0-7.5-7.45 7.46 7.46 0 0 0 7.5 7.45zm0-10.91a3.45 3.45 0 1 1-3.5 3.46A3.46 3.46 0 0 1 14.17 4zM14.17 16.47A14.18 14.18 0 0 0 0 30.68c0 1.41.66 4 5.11 5.66a27.17 27.17 0 0 0 9.06 1.34c6.54 0 14.17-1.84 14.17-7a14.18 14.18 0 0 0-14.17-14.21zm0 17.21c-6.3 0-10.17-1.77-10.17-3a10.17 10.17 0 1 1 20.33 0c.01 1.23-3.86 3-10.16 3z"></path>
+                </svg>
+            </a>
+            <a href="/cart" class="header_goods"><i class="el-icon-goods"></i></a>
         </div>
         <div class="grid_lists">
             <nav>
@@ -87,7 +92,7 @@ export default {
         .search-input {
             width: 100%;
             max-width: 45px;
-            padding-right: 50px;
+            padding-right: 80px;
             transition: all 0.35s cubic-bezier(0.29, 0.63, 0.44, 1);
             -moz-transition: all 0.35s cubic-bezier(0.29, 0.63, 0.44, 1);
             -webkit-transition: all 0.35s cubic-bezier(0.29, 0.63, 0.44, 1);
@@ -103,11 +108,20 @@ export default {
                 }
             }
         }
-        .el-icon-goods {
+        .header_goods {
             position: absolute;
             top: 50%;
             margin-top: -10px;
             right: 0;
+        }
+        .header_account {
+            position: absolute;
+            top: 50%;
+            right: 50px;
+            padding: 8px 0;
+            -ms-transform: translateY(-50%);
+            -webkit-transform: translateY(-50%);
+            transform: translateY(-50%);
         }
     }
     .grid_lists {
---
+++ src/components/Login.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="login-page text-center">
+        <el-card class="box-card">
+            <el-form status-icon ref="ruleForm2" label-width="100px" class="demo-ruleForm">
+                <el-form-item label="用户名">
+                    <el-input type="text" v-model="name" autoComplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="密码">
+                    <el-input type="password" v-model="passwd" autoComplete="off"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="login">登录</el-button>
+                </el-form-item>
+            </el-form>
+        </el-card>
+        <Footer></Footer>
+    </div>
+
+</template>
+<script>
+import Footer from './Footer.vue'
+
+export default {
+  name: 'Login',
+  components: {
+    Footer
+  },
+  data: function () {
+    return {
+      name: '',
+      passwd: ''
+    }
+  },
+  methods: {
+    login: function () {
+      console.log('loginclicked')
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+  .login-page {
+    padding-top: 100px;
+    height: 100%;
+    width: 100%;
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    padding: 18px 0;
+  }
+
+  .box-card {
+    width: 480px;
+    margin: 0 auto;
+  }
+</style>
